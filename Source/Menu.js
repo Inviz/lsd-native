500bb6e37f693cca824f20d2d9c6bbf1d555e152
Yaroslaff Fedin <me@inviz.ru>
2011-03-18 20:55:37 +0800

    Add menu natives

---
+++ Source/Menu.js
@@ -0,0 +1,27 @@
+/*
+---
+ 
+script: Menu.js
+ 
+description: A generic menu
+ 
+license: Public domain (http://unlicense.org).
+
+authors: Yaroslaff Fedin
+ 
+requires:
+  - LSD/LSD.Native
+
+provides: 
+  - LSD.Native.Menu
+ 
+...
+*/
+
+LSD.Native.Menu = new Class({
+  Extends: LSD.Native,
+  
+  options: {
+    tag: 'menu'
+  }
+})
